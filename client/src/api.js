6220c5d7d6fd6538ef270187ce3595bbfff1ae7d
MOHAMMAD ARFAT <mdarfatyunus@gmail.com>
2025-04-19 23:04:52 +0600

    Added frontend env for backend connection

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,14 @@
 import axios from "axios";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api", 
 });
+
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
   if (token) req.headers.Authorization = token;
   return req;
 });
+
 export default API;
